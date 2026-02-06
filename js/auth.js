ff9d103e4a14a463757a218f36428ae70b2141b4
pho9902 <bagh9902@gmail.com>
2026-02-06 14:24:19 +0900

    feat: nickname input focus out 시 빈 문자열인지 검사 추가

--- js/auth.js
+++ js/auth.js
@@ -2,19 +2,20 @@ const emailInput = document.querySelector("#emailInput");
 const emailContainer = document.querySelector("#emailInputContainer");
 const emailError = document.querySelector("#emailError");
 
+const nameInput = document.querySelector("#nameInput");
+const nameContainer = document.querySelector("#nameInputContainer");
+const nameError = document.querySelector("#nicknameError");
+
 function checkEmail(email) {
   if (!email.includes("@")) return false;
-
   const parts = email.split("@");
   if (parts.length !== 2) return false;
   if (!parts[1].includes(".")) return false;
-
   return true;
 }
 
 emailInput.addEventListener("focusout", () => {
   const value = emailInput.value;
-
   if (!value) {
     emailContainer.classList.add("error");
     emailError.textContent = "이메일을 입력해주세요.";
@@ -26,5 +27,16 @@ emailInput.addEventListener("focusout", () => {
   } else {
     emailContainer.classList.remove("error");
     emailError.style.display = "none";
+  }
+});
+
+nameInput.addEventListener("focusout", () => {
+  if (!nameInput.value) {
+    nameContainer.classList.add("error");
+    nameError.textContent = "닉네임을 입력해주세요.";
+    nameError.style.display = "block";
+  } else {
+    nameContainer.classList.remove("error");
+    nameError.style.display = "none";
   }
 });
