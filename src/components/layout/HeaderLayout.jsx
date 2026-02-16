9626cb7ad2451405a4aeb38b512223d93e0476a5
Hanpla <hanpla25@gmail.com>
2026-02-16 12:09:52 +0900

    style: Header 스타일 sticky 추가

--- src/components/layout/HeaderLayout.jsx
+++ src/components/layout/HeaderLayout.jsx
@@ -1,7 +1,7 @@
 export default function HeaderLayout({ children }) {
   return (
-    <header className="border-b border-[#DFDFDF] h-17.5">
-      <div className="flex justify-between items-center pt-2.5 px-4 md:px-6 max-w-280 mx-auto">
+    <header className="sticky top-0 z-1000 bg-white border-b border-[#DFDFDF] h-17.5">
+      <div className="flex justify-between items-center h-full px-4 md:px-6 max-w-280 mx-auto">
         {children}
       </div>
     </header>
