4496573b700701ecd67244aaf7de3323a69e7c3b
Hanpla <hanpla25@gmail.com>
2026-02-18 16:20:30 +0900

    fix: 판다마켓 로고 범위 수정

--- src/components/auth/AuthLayout.jsx
+++ src/components/auth/AuthLayout.jsx
@@ -5,14 +5,10 @@ import logoImg from "../../assets/login-form-logo.svg";
 
 export default function AuthLayout({ children }) {
   return (
-    <main className="min-h-screen bg-white flex flex-col items-center px-4 py-20 lg:py-40">
+    <main className="min-h-screen bg-white flex flex-col items-center px-4 py-10 lg:py-20">
       <div className="w-full max-w-100 md:max-w-160 flex flex-col items-center">
-        <Link to="/">
-          <img
-            src={logoImg}
-            alt="판다마켓"
-            className="w-49.5 md:w-52.5 mb-10"
-          />
+        <Link to="/" className="mb-10">
+          <img src={logoImg} alt="판다마켓" className="w-49.5 md:w-52.5" />
         </Link>
         {children}
       </div>
