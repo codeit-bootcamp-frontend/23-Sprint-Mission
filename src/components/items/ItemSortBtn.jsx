86df4e7c7fb89523579c1746c0ce3434619d107b
Hanpla <hanpla25@gmail.com>
2026-02-19 20:19:56 +0900

    refactor: 단순 변수 이름 변경

--- src/components/items/ItemSortBtn.jsx
+++ src/components/items/ItemSortBtn.jsx
@@ -18,7 +18,7 @@ export default function ItemSortBtn() {
   ];
 
   const selectedLabel =
-    options.find((opt) => opt.value === currentOrder)?.label || "최신순";
+    options.find((option) => option.value === currentOrder)?.label || "최신순";
 
   const handleSortChange = (newOrder) => {
     searchParams.set("orderBy", newOrder);
@@ -63,7 +63,7 @@ const LargeSortBtn = ({ onClick, selected }) => {
   return (
     <button
       onClick={onClick}
-      className="hidden md:flex items-center justify-between gap-2 h-10.5 px-4 bg-white border border-gray-200 rounded-xl min-w-25 md:min-w-30 transition-all hover:bg-gray-50 cursor-pointer"
+      className="hidden md:flex items-center justify-between gap-2 h-10.5 px-4 bg-white border border-gray-200 rounded-xl min-w-30 transition-all hover:bg-gray-50 cursor-pointer"
     >
       <span className="hidden md:block text-[14px] font-medium text-gray-800">
         {selected}
