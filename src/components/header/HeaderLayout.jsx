3586c69f53acd4b7959d4a9c77918b49af238a41
Hanpla <hanpla25@gmail.com>
2026-02-19 18:04:38 +0900

    style: px 변경

--- src/components/header/HeaderLayout.jsx
+++ src/components/header/HeaderLayout.jsx
@@ -1,6 +1,6 @@
 export default function HeaderLayout({ children, maxWidth }) {
   return (
-    <header className="sticky top-0 z-1000 bg-white border-b border-[#DFDFDF] h-17.5 px-5">
+    <header className="sticky top-0 z-1000 bg-white border-b border-[#DFDFDF] h-17.5 px-4">
       <div
         className="flex justify-between items-center h-full lg:mx-auto"
         style={{ maxWidth: maxWidth }}
