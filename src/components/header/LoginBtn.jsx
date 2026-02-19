40587ba3ac7d26ac74f8032b0079f5b875450583
Hanpla <hanpla25@gmail.com>
2026-02-19 13:17:30 +0900

    feat: Header 컴포넌트 인덱스 페이지와 다른 페이지 스타일 다르게

--- src/components/header/LoginBtn.jsx
+++ src/components/header/LoginBtn.jsx
@@ -1,9 +1,27 @@
-// Components
 import LinkBtn from "../btns/LinkBtn";
 
-export default function LoginBtn() {
+const INDEX_BTN_STYLE = {
+  width: "128px",
+  height: "48px",
+};
+
+const OTHER_BTN_STYLE = {
+  width: "88px",
+  height: "42px",
+};
+
+export default function LoginBtn({ isMainPage }) {
+  const currentStyle = isMainPage ? INDEX_BTN_STYLE : OTHER_BTN_STYLE;
+
   return (
-    <LinkBtn href="/login" className="bg-primary w-32 h-12 rounded-lg">
+    <LinkBtn
+      href="/login"
+      className="bg-primary flex items-center justify-center rounded-lg text-white font-semibold"
+      style={{
+        width: currentStyle.width,
+        height: currentStyle.height,
+      }}
+    >
       로그인
     </LinkBtn>
   );
