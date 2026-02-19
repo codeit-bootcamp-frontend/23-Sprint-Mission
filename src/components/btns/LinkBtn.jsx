888bdc0b30cce5648eb92289d6bd5dd52bd32306
Hanpla <hanpla25@gmail.com>
2026-02-19 13:16:54 +0900

    chore: 링크 버튼 스타일 외부에서 받도록 변경

--- src/components/btns/LinkBtn.jsx
+++ src/components/btns/LinkBtn.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router";
 
-export default function LinkBtn({ children, href, className }) {
+export default function LinkBtn({ children, href, className, style }) {
   return (
     <Link
       to={href}
       className={`flex items-center justify-center leading-6.5 font-semibold text-[#F3F4F6] ${className}`}
+      style={style}
     >
       {children}
     </Link>
