cb30514623291ae48aa03a62a0e492032f6e1fe6
Hanpla <hanpla25@gmail.com>
2026-02-19 14:05:24 +0900

    feat: getProducts 함수 추가

---
+++ src/libs/api/product.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+const instance = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const getProducts = async (pageSize = 10, orderBy = "recent") => {
+  try {
+    const response = await instance.get("/products", {
+      params: {
+        pageSize,
+        orderBy,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(`API 호출 에러:`, error);
+    throw error;
+  }
+};
