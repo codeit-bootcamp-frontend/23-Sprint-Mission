3a8a42e1b623a2093529382aa35aaeb7a36ab2df
Hanpla <hanpla25@gmail.com>
2026-02-20 10:55:13 +0900

    fix: 페이지네이션 버그 수정

--- src/components/items/ItemPagination.jsx
+++ src/components/items/ItemPagination.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router";
+import { Link, useSearchParams } from "react-router";
 
 // Imges
 import arrowLeft from "../../assets/arrow_left.svg";
@@ -8,19 +8,32 @@ import arrowRight from "../../assets/arrow_right.svg";
 import { generatePages } from "../../libs/utils/pagination";
 
 export default function ItemPagination({ currentPage, totalPage }) {
+  const [searchParams] = useSearchParams();
   const pages = generatePages(currentPage, totalPage);
+
+  const getPageLink = (pageNumber) => {
+    const params = new URLSearchParams(searchParams);
+    params.set("page", pageNumber);
+    return `?${params.toString()}`;
+  };
+
   const prevPage = Math.max(1, currentPage - 1);
   const nextPage = Math.min(totalPage, currentPage + 1);
 
   return (
     <div className="flex justify-center gap-1">
-      <PaginationArrow direction="left" href={`?page=${prevPage}`} />
+      <PaginationArrow direction="left" href={getPageLink(prevPage)} />
 
       {pages.map((num) => (
-        <PageNumber key={num} number={num} isActive={num === currentPage} />
+        <PageNumber
+          key={num}
+          number={num}
+          isActive={num === currentPage}
+          href={getPageLink(num)}
+        />
       ))}
 
-      <PaginationArrow direction="right" href={`?page=${nextPage}`} />
+      <PaginationArrow direction="right" href={getPageLink(nextPage)} />
     </div>
   );
 }
@@ -40,14 +53,14 @@ const PaginationArrow = ({ direction, href }) => {
   );
 };
 
-const PageNumber = ({ number, isActive }) => {
+const PageNumber = ({ number, isActive, href }) => {
   return (
     <Link
-      to={`?page=${number}`}
+      to={href}
       className={`flex items-center justify-center w-10 h-10 rounded-full border
         ${
           isActive
-            ? "bg-[#2F80ED] text-white border-[#2F80ED] font-bold"
+            ? "bg-primary text-white border-primary font-bold"
             : "bg-white text-gray-600 border-gray-200"
         }`}
     >
