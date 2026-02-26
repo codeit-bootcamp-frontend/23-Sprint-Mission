dbf8cf048d65f358a9e2e592c24a412b6bdc3fad
Hanpla <hanpla25@gmail.com>
2026-02-26 09:32:05 +0900

    feat: 페이지의 아이템 요청 개수를 화면 크기에 따라 다르게 변경

--- src/libs/utils/pagination.js
+++ src/libs/utils/pagination.js
@@ -16,7 +16,7 @@ export const generatePages = (currentPage, totalPages) => {
 
 export const getPageSizes = () => {
   const width = window.innerWidth;
-  if (width >= 1024) return { best: 4, all: 10 }; // Desktop
-  if (width >= 768) return { best: 2, all: 6 }; // Tablet
+  if (width >= 1024) return { BEST_PAGE_SIZE: 4, ALL_PAGE_SIZE: 10 }; // Desktop
+  if (width >= 768) return { BEST_PAGE_SIZE: 2, ALL_PAGE_SIZE: 6 }; // Tablet
   return { BEST_PAGE_SIZE: 1, ALL_PAGE_SIZE: 4 }; // Mobile
 };
