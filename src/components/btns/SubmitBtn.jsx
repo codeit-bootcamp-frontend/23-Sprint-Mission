901c08f5877f783fa4ffe166d44a530938b732bd
Hanpla <hanpla25@gmail.com>
2026-02-26 16:07:53 +0900

    chore: SubmitBtn 컴포넌트 스타일 변경

--- src/components/btns/SubmitBtn.jsx
+++ src/components/btns/SubmitBtn.jsx
@@ -1,9 +1,9 @@
-export default function SubmitBtn({ children, disabled }) {
+export default function SubmitBtn({ children, disabled, className }) {
   return (
     <button
       type="submit"
       disabled={disabled}
-      className="w-full py-3 bg-primary text-gray-100 rounded-[40px] font-semibold text-xl leading-8 disabled:bg-gray-400 disabled:cursor-not-allowed"
+      className={`disabled:bg-gray-400 disabled:cursor-not-allowed ${className}`}
     >
       {children}
     </button>
