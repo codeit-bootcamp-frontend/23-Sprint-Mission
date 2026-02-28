e46780a94bae297455c11d7f40994cae62272870
Hanpla <hanpla25@gmail.com>
2026-02-28 09:09:47 +0900

    refactor: 아이템 검색 인풋 구조 변경

--- src/components/items/ItemSearchInput.jsx
+++ src/components/items/ItemSearchInput.jsx
@@ -1,14 +1,28 @@
-import { useSearchParams } from "react-router";
+import { useSearchParams, useNavigate } from "react-router";
 
 // Imges
 import SearchIcon from "../../assets/icons/ic_search.svg";
 
 export default function ItemSearchInput() {
   const [searchParams, _] = useSearchParams();
-  const currentOrderBy = searchParams.get("orderBy");
+  const navigate = useNavigate();
+
+  const currentOrderBy = searchParams.get("orderBy") || "recent";
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const keyword = formData.get("keyword");
+
+    navigate(`/items?keyword=${keyword}&orderBy=${currentOrderBy}`);
+  };
 
   return (
-    <form action="/items" className="relative w-full md:w-60.5 lg:w-81.25">
+    <form
+      onSubmit={handleSubmit}
+      className="relative w-full md:w-60.5 lg:w-81.25"
+    >
       <div className="absolute left-4 top-1/2 -translate-y-1/2 pointer-events-none">
         <img src={SearchIcon} alt="검색" />
       </div>
@@ -17,14 +31,8 @@ export default function ItemSearchInput() {
         type="text"
         name="keyword"
         placeholder="검색할 상품을 입력해주세요"
-        className="pl-11 pr-4 w-full h-10.5 bg-gray-100 rounded-xl text-[16px] text-gray-800 placeholder:text-gray-400 outline-none focus:ring-1 focus:ring-primary transition-all"
+        className="pl-11 pr-4 w-full h-10.5 bg-gray-100 rounded-xl text-[16px] text-gray-800 outline-none"
       />
-
-      {currentOrderBy && <HiddenInput currentOrderBy={currentOrderBy} />}
     </form>
   );
 }
-
-const HiddenInput = ({ currentOrderBy }) => (
-  <input type="hidden" name="orderBy" value={currentOrderBy} />
-);
