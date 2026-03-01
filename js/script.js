9d25be02cde82def2c18bded89e288027ff0c747
isuzzi <isuzzi11@gmail.com>
2026-03-01 23:22:30 +0900

    feature: 로그인 비밀번호 유효성 검사 스트립트 추가

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-//이메일 검증
 const useremail = document.getElementById('email');
 const userpw = document.getElementById('password');
 const errorEmail = document.getElementById("email-error");
+const errorPw = document.getElementById("password-error");
 
+//이메일 검증
 function validationEmail (){
   const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
   const isValid = emailRegex.test(useremail.value);
@@ -24,3 +25,20 @@ function validationEmail (){
 
 }
 useremail.addEventListener('blur', validationEmail); //함수 "밖"에서 DOM 요소를 가져온 뒤 이벤트와 연결하는 용도로 쓴다.
+
+//비밀번호 검증
+function validationPw (){
+  const value = userpw.value.trim();
+  //빈 값 먼저 검사
+  if (value === "") {
+    errorPw.textContent = "";
+    return; 
+  }else if (value.length < 8){
+    userpw.classList.add('error');
+    return errorPw.textContent = "비밀번호를 8자 이상 입력하세요.";
+  }else{
+    userpw.classList.remove('error');
+    return errorPw.textContent = "";
+  }
+}
+userpw.addEventListener('blur', validationPw); //함수 "밖"에서 DOM 요소를 가져온 뒤 이벤트와 연결하는 용도로 쓴다.
