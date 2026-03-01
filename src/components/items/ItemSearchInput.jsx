2974353a04d7c6fea8fd2b01d4ea95a4d24ccdeb
Hanpla <hanpla25@gmail.com>
2026-03-01 16:46:06 +0900

    refactor: ItemSearchInput form onSubmit 대신 action으로 변경

--- src/components/items/ItemSearchInput.jsx
+++ src/components/items/ItemSearchInput.jsx
@@ -1,26 +1,21 @@
 import { useSearchParams, useNavigate } from "react-router";
-
-// Imges
 import SearchIcon from "../../assets/icons/ic_search.svg";
 
 export default function ItemSearchInput() {
-  const [searchParams, _] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const navigate = useNavigate();
 
   const currentOrderBy = searchParams.get("orderBy") || "recent";
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    const formData = new FormData(e.currentTarget);
+  const handleSearchAction = (formData) => {
     const keyword = formData.get("keyword");
 
-    navigate(`/items?keyword=${keyword}&orderBy=${currentOrderBy}`);
+    navigate(`/items?keyword=${keyword || ""}&orderBy=${currentOrderBy}`);
   };
 
   return (
     <form
-      onSubmit={handleSubmit}
+      action={handleSearchAction}
       className="relative w-full md:w-60.5 lg:w-81.25"
     >
       <div className="absolute left-4 top-1/2 -translate-y-1/2 pointer-events-none">
