7595d28da9834a0b90a72ffb226ab52e6d50f0c1
Hanpla <hanpla25@gmail.com>
2026-03-26 14:47:52 +0900

    refactor: 타입스크립트 마이그레이션 시작

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./index.css";
+
+import MainLayout from "./components/layout/MainLayout";
+// Pages
+import Home from "./pages/Home";
+import LoginPage from "./pages/Login";
+import SignupPage from "./pages/Signup";
+import ItemsPage from "./pages/ItemsPage";
+import AddItem from "./pages/AddItem";
+import ProductPage from "./pages/Product";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="items">
+            <Route index element={<ItemsPage />} />
+            <Route path=":productId" element={<ProductPage />} />
+          </Route>
+          <Route path="additem" element={<AddItem />} />
+        </Route>
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,10 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
-import "./index.css";
 
-// Layout
-import MainLayout from "./components/layout/MainLayout";
-
-// Pages
-import Home from "./pages/Home";
-import LoginPage from "./pages/Login";
-import SignupPage from "./pages/Signup";
-import ItemsPage from "./pages/ItemsPage";
-import AddItem from "./pages/AddItem";
-import ProductPage from "./pages/Product";
+import App from "@/App";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route path="items">
-          <Route index element={<ItemsPage />} />
-          <Route path=":productId" element={<ProductPage />} />
-        </Route>
-        <Route path="additem" element={<AddItem />} />
-      </Route>
-
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-    </Routes>
-  </BrowserRouter>,
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
