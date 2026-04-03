3c79e0bfc00984558c4926b4a2c7cef75c643888
Hanpla <hanpla25@gmail.com>
2026-04-03 16:43:32 +0900

    chore: 기본 폰트 설정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,20 @@
 import type { Metadata } from "next";
+import { Abhaya_Libre } from "next/font/google";
+import localFont from "next/font/local";
 import "@/libs/styles/globals.css";
+
+const pretendard = localFont({
+  src: "../node_modules/pretendard/dist/web/variable/woff2/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
+
+const abhaya = Abhaya_Libre({
+  subsets: ["latin"],
+  weight: "800",
+  variable: "--font-abhaya",
+});
 
 export const metadata: Metadata = {
   title: "판다마켓",
@@ -12,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={`${pretendard.variable} ${abhaya.variable}`}>
       <body>{children}</body>
     </html>
   );
