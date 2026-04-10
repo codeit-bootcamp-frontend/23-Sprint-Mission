eed8cd641b18cf31fae85267c695d8618d0c1e9a
Hanpla <hanpla25@gmail.com>
2026-04-10 10:04:52 +0900

    chore: 초기화

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,8 @@
 import type { Metadata } from "next";
-import { Abhaya_Libre } from "next/font/google";
-import localFont from "next/font/local";
-import "@/libs/styles/globals.css";
-
-const pretendard = localFont({
-  src: "../node_modules/pretendard/dist/web/variable/woff2/PretendardVariable.woff2",
-  display: "swap",
-  weight: "45 920",
-  variable: "--font-pretendard",
-});
-
-const abhaya = Abhaya_Libre({
-  subsets: ["latin"],
-  weight: "800",
-  variable: "--font-abhaya",
-});
 
 export const metadata: Metadata = {
-  title: "판다마켓",
-  description: "판다마켓",
+  title: "Do it",
+  description: "투두 리스트",
 };
 
 export default function RootLayout({
@@ -27,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className={`${pretendard.variable} ${abhaya.variable}`}>
+    <html lang="ko">
       <body>{children}</body>
     </html>
   );
