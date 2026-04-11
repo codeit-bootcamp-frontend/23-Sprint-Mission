5d927a25623fae21d4861a3faf0329d15b971aec
Hanpla <hanpla25@gmail.com>
2026-04-11 10:04:41 +0900

    style: 기본 폰트 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,24 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "@/libs/styles/globals.css";
+
+const nanumSquare = localFont({
+  src: [
+    {
+      path: "../assets/fonts/NanumSquareR.ttf",
+      weight: "400",
+    },
+    {
+      path: "../assets/fonts/NanumSquareB.ttf",
+      weight: "700",
+    },
+    {
+      path: "../assets/fonts/NanumSquareEB.ttf",
+      weight: "800",
+    },
+  ],
+  variable: "--font-nanum",
+});
 
 export const metadata: Metadata = {
   title: "Do it",
@@ -11,8 +31,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
-      <body>{children}</body>
+    <html lang="ko" className={`${nanumSquare.variable}`}>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
