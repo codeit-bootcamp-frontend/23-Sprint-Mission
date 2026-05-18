1c364b30eb18564ba8a22c62a53b2f614b4209b1
chaewon <devchae10@gmail.com>
2026-05-18 16:37:33 +0900

    feat: 상품 등록 폼 관련 커스텀훅 생성

--- src/apis/comment/getComments.js
+++ src/apis/comment/getComments.js
@@ -3,7 +3,7 @@ import instance from '../instance';
 export const getComments = async (productId, cursor) => {
   const { data } = await instance.get(`/products/${productId}/comments`, {
     params: {
-      limit: 5,
+      limit: 500,
       cursor,
     },
   });
