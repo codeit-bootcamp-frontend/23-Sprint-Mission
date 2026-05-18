269e3c416100ed435acfbe7f1577e53691ad8661
chaewon <devchae10@gmail.com>
2026-05-18 17:34:27 +0900

    feat: 상품 등록 api 연동하기

--- src/pages/AddItem.jsx
+++ src/pages/AddItem.jsx
@@ -1,10 +1,14 @@
-import { Inner } from '../styles/layout';
+import { useNavigate } from 'react-router-dom';
+import { createProduct } from '../apis/product/createProduct';
 import useProductForm from '../hooks/useProductForm';
 import ProductForm from '../components/form/ProductForm';
 import styled from 'styled-components';
+import { Inner } from '../styles/layout';
 import { DEVICE } from '../styles/breakpoints';
 
 function AddItem() {
+  const navigate = useNavigate();
+
   const {
     formValues,
     formattedPrice,
@@ -14,8 +18,25 @@ function AddItem() {
     isFormValid,
   } = useProductForm();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const productData = {
+      images: [],
+      tags,
+      price: Number(formValues.price),
+      description: formValues.description,
+      name: formValues.productName,
+    };
+
+    try {
+      const createdProduct = await createProduct(productData);
+
+      alert('상품이 등록되었습니다.');
+      navigate(`/items/${createdProduct.id}`);
+    } catch (error) {
+      console.error('상품 등록 실패', error);
+    }
   };
 
   return (
