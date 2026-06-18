89ea8dcf07c909171653b5ae7db22b23331010ce
chaewon <devchae10@gmail.com>
2026-06-19 01:44:35 +0900

    react query import 추가

--- src/pages/Items/index.jsx
+++ src/pages/Items/index.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import styled from 'styled-components';
 import { getListProducts } from '../apis/product/getListProducts';
 import { toggleFavoriteApi } from '../utils/favorite/favoriteApi';
